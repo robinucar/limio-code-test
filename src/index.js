1da00d67a7b41b0c187253e0ef0dc54a0289953c
Robin Winters <robinmelsaw@gmail.com>
2024-06-30 23:53:36 +0100

    upgrade and update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import "./wdyr"
+import './wdyr';
 
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById("root"))
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// Get the root element from the DOM
+const container = document.getElementById('root');
+
+// Create a root
+const root = createRoot(container);
+
+// Initial render
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
